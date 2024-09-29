94172dfb54df95405db95e9dc17db481071155a8
а <@DESKTOP-FBV613P>
2024-09-29 22:01:35 +0500

    ///

--- дз1/Program.cs
+++ дз1/Program.cs
@@ -11,17 +11,18 @@ namespace дз1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Отрывок А.С.Пушкина",
-                ConsoleColor.Green);
-            Console.WriteLine("Унылая пора! Очей очарованье!"
-            "Приятна мне твоя прощальная краса —"
-            "Люблю я пышное природы увяданье,"
-            "В багрец и в золото одетые леса,"
-            "В их сенях ветра шум и свежее дыханье,"
-            "И мглой волнистою покрыты небеса,"
-            "И редкий солнца луч, и первые морозы,"
-            "И отдаленные седой зимы угрозы.",
-            ConsoleColor.Yellow);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Отрывок А.С.Пушкина");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Унылая пора! Очей очарованье!");
+            Console.WriteLine("Приятна мне твоя прощальная краса —");
+            Console.WriteLine("Люблю я пышное природы увяданье,");
+            Console.WriteLine("В багрец и в золото одетые леса,");
+            Console.WriteLine("В их сенях ветра шум и свежее дыханье,");
+            Console.WriteLine("И мглой волнистою покрыты небеса,");
+            Console.WriteLine("И редкий солнца луч, и первые морозы,");
+            Console.WriteLine("И отдаленные седой зимы угрозы.");
+            Console.ResetColor();   
             Console.ReadKey();
         }
     }
