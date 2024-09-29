4516f5fbb71a98261165469e9c9d8cd68e7a9d40
а <@DESKTOP-FBV613P>
2024-09-29 20:10:01 +0500

    Добавьте файлы проекта.

---
+++ дз1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace дз1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Отрывок А.С.Пушкина",
+                ConsoleColor.Green);
+            Console.WriteLine("Унылая пора! Очей очарованье!"
+            "Приятна мне твоя прощальная краса —"
+            "Люблю я пышное природы увяданье,"
+            "В багрец и в золото одетые леса,"
+            "В их сенях ветра шум и свежее дыханье,"
+            "И мглой волнистою покрыты небеса,"
+            "И редкий солнца луч, и первые морозы,"
+            "И отдаленные седой зимы угрозы.",
+            ConsoleColor.Yellow);
+            Console.ReadKey();
+        }
+    }
+}
