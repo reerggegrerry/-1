4d9acdd60cafe1611e7fa67e8fc8a0b02abcfc54
а <@DESKTOP-FBV613P>
2024-10-07 09:27:11 +0500

    сделал

---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первый угол при стороне");
+            double alpha = Math.PI * double.Parse(Console.ReadLine()) / 180;
+
+            Console.WriteLine("Введите второй угол при стороне");
+            double beta = Math.PI * double.Parse(Console.ReadLine()) / 180;
+
+            double s = (a*a*Math.Sin(alpha)) / (2*Math.Sin(alpha+beta));
+            Console.WriteLine("Площадь: " + s);
+            Console.ReadKey();
+            //найти площадь треугольника по стороне и двум углам. Вар9
+        }
+    }
+}
